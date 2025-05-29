9ad33530bc3abe8c170beea6de52a137e982b69b
Prashant Dixit <prashantdixit1650@gmail.com>
2025-05-29 05:26:47 +0000

    Memories in JS

---
+++ memories.js
@@ -0,0 +1,13 @@
+//Stack and Heap memory
+// stack - primitive types 
+// heap - non primitive types
+
+let name = "Kartic"
+let anothername = name
+anothername = "rahul"
+
+console.log(name);
+console.log(anothername);
+
+
+
