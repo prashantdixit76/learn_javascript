cd66f1aa8aef05dc0328c652aa275389ed14a41c
Prashant Dixit <prashantdixit1650@gmail.com>
2025-05-29 05:43:19 +0000

    memories in JS

--- memories.js
+++ memories.js
@@ -2,12 +2,30 @@
 // stack - primitive types 
 // heap - non primitive types
 
+const { useInsertionEffect } = require("react");
+
 let name = "Kartic"
 let anothername = name
 anothername = "rahul"
 
 console.log(name);
 console.log(anothername);
+
+
+
+// heap
+let userone =  {
+    email: "kartic@gmail.com",
+    Upi: "kartic@ybl"
+
+}
+
+let usertwo = userone
+
+usertwo.email = "prashant@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email)
 
 
 
