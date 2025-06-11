474acc04fc69566d6e67f5a98622a940aead4982
Prashant Dixit <prashantdixit1650@gmail.com>
2025-06-11 16:26:27 +0000

    IIFE in js

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,16 @@
+// (IIFE) Immediately Invoked Function Expression
+
+//named iifi
+(function chai(){
+    console.log(`DB CONNECT`);
+})();
+
+
+
+//by arrow function 
+//unnamed iifi
+((name) => {
+    console.log(`DB CONNECT TWO ${name}`);
+})('kartic')
+
+
