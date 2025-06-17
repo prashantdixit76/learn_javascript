479ff4f73032646df7a6cc9a2075d83a3936f373
Prashant Dixit <prashantdixit1650@gmail.com>
2025-06-17 11:37:41 +0000

    while and dowhile loop in js

---
+++ 05_iterations/02_while&dowhile.js
@@ -0,0 +1,28 @@
+//while
+let i = 0
+while (i <= 10) {
+    console.log(`value of index is ${i}`);
+    i = i + 2
+}
+
+
+
+let myarray = ["karan" , "rahul" , "rohan"]
+let arr = 0
+while (arr < myarray.length){
+    console.log(`value is ${myarray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+
+
+// --------------------------------
+// doWhile loop
+
+let score = 1 
+do {
+    console.log(`score is ${score}`);
+    score++
+}while(score <= 10);
